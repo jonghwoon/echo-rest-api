8023d93472faf68f4b89d88489a64c3d696dabda
jonghoon.lee <greatest86@gmail.com>
2026-05-20 14:14:06 +0900

    router.go에 CookieSecure 추가

--- router/router.go
+++ router/router.go
@@ -23,6 +23,7 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
+		CookieSecure:   true,
 		CookieSameSite: http.SameSiteNoneMode,
 		//CookieSameSite: http.SameSiteDefaultMode,
 		//CookieMaxAge:   60,
